ad3747ca45e112093ec7b31a6c46c351fa58fd66
MafKamRAN <143469368+MafKamRAN@users.noreply.github.com>
2024-07-23 03:45:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const mouseEvent = document.documentElement;
+mouseEvent.addEventListener("mousemove", (e) => {
+  mouseEvent.style.setProperty("--x", e.clientX + "px");
+  mouseEvent.style.setProperty("--y", e.clientY + "px");
+});
